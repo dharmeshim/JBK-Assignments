c3eab8d70c5f5394e6d6a5f5808f309d8774b7e2
DHARMESH PRASAD <98016812+dharmeshim@users.noreply.github.com>
2023-08-20 12:33:33 +0530

    Inheritance-based Employee System in Java

---
+++ employeesystem/Developer.java
@@ -0,0 +1,17 @@
+package employeesystem;
+
+public class Developer extends Employee {
+
+	private String programmingLanguage;
+
+	public Developer(int id, String name, String programmingLanguage) {
+		super(id, name);
+		this.programmingLanguage = programmingLanguage;
+	}
+
+	public void getDetails() {
+		System.out.println("Employee ID : " + getId() + "\nEmployee name : " + getName()
+				+ "\nDeveloper Programming Language : " + programmingLanguage);
+	}
+
+}
---
+++ employeesystem/Manager.java
@@ -0,0 +1,18 @@
+package employeesystem;
+
+public class Manager extends Employee {
+
+	private String department;
+
+	public Manager(int id, String name, String department) {
+		super(id, name);
+		this.department = department;
+
+	}
+
+	public void getDetails() {
+		System.out.println("Employee ID : " + getId() + "\nEmployee name : " + getName() + "\nManager of " + department
+				+ " department");
+	}
+
+}
