ce4e27df3103e147c9beffd3414af3052acd9f88
DHARMESH PRASAD <98016812+dharmeshim@users.noreply.github.com>
2023-08-20 12:37:09 +0530

    Implementing Interfaces in Java for Vehicles

---
+++ vehicles/Vehicle.java
@@ -0,0 +1,11 @@
+package vehicles;
+
+public interface Vehicle {
+
+	public abstract void start();
+
+	public abstract void accelerate();
+
+	public abstract void brake();
+
+}
---
+++ vehicles/VehicleInteraction.java
@@ -0,0 +1,21 @@
+package vehicles;
+
+public class VehicleInteraction {
+    public static void main(String[] args) {
+        Vehicle car = new Car();
+        Vehicle motorcycle = new Motorcycle();
+        
+        car.start();
+        car.accelerate();
+        car.brake();
+        
+        motorcycle.start();
+        motorcycle.accelerate();
+        motorcycle.brake();
+        
+     
+
+       
+    }
+}
+
